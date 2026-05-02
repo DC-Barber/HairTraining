77ab6ded89cba0c3e901e28b389de6a5c319833d
DC-Barber <wizza.burma@gmail.com>
2026-05-02 22:44:07 +0630

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     HISTORY_SHEET_API: 'https://script.google.com/macros/s/AKfycbxEarnFSqXxG16vLEKJ7nwbCQcGNbQTEf7a-XVzSuuEgDY5DHqcwJ4uIraqK0x-ZzYL/exec',
     
     // ✅ ဒီ URL ကို အပေါ်က Deploy ပြီးရင် ရလာတဲ့ URL နဲ့ အစားထိုးပါ
-    IMGBB_PROXY_URL: 'https://script.google.com/macros/s/AKfycbzx-R1V07MXQxYjLT5gH3EK6KR23UYuCWATY4oEc2bZieNduAiDpU3neeJyXMSzOjT4EA/exec',
+    IMGBB_PROXY_URL: 'https://script.google.com/macros/s/AKfycbz19kwqW19XVo4fKENRn3nGztoUIuRiMwK7foH8qs1Xm1LlaKhh3JQdnKm3NxRzjXiZoA/exec',
     
     IMGBB_API_KEY: 'e91fc2f91dc6bb15046d0684d17f6c6b',
     
