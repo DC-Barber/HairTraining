c1404bb30b3d48b8f7e815c0d20be027398d05d1
DC-Barber <wizza.burma@gmail.com>
2026-05-03 01:55:37 +0630

    Add files via upload

---
+++ chat/config.js
@@ -0,0 +1,14 @@
+// config.js
+const CONFIG = {
+    USERS_SHEET_API: 'https://script.google.com/macros/s/AKfycbykNsXYHHBzHZ0-bFrFVLon1yKcMGPY2t9Z3d659BvtFlBVjzQURo8So1UKMa2wMnPO/exec',
+    HISTORY_SHEET_API: 'https://script.google.com/macros/s/AKfycbxEarnFSqXxG16vLEKJ7nwbCQcGNbQTEf7a-XVzSuuEgDY5DHqcwJ4uIraqK0x-ZzYL/exec',
+    IMGBB_PROXY_URL: 'https://script.google.com/macros/s/AKfycbwVqRvK9SoRpFz3NuTM5f5pSKUxVc5dNPNGZSXvrdbmAv4H6f2kGR-0LGHbLOGX145Erw/exec',
+    IMGBB_API_KEY: 'e91fc2f91dc6bb15046d0684d17f6c6b',
+    
+    // ✅ NEW - Chat API URL (အပေါ်က Deploy လုပ်ထားတဲ့ URL ကိုထည့်)
+    CHAT_API_URL: 'https://script.google.com/macros/s/AKfycbybDbfxKvPH0_t2Xm1PLaiNXlJrPQa4_nIb041waHcyjLGt4HMXHmvg2d2Itk9fnzt0pw/exec',
+    
+    AUTH_EXPIRY_KEY: 'hair_auth_expiry',
+    USER_DATA_KEY: 'hair_user_data',
+    LOGIN_DURATION_MS: 86400000
+};
