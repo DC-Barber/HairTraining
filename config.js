aa4e001175fe79c5e3098751a395ea8c5622db07
DC-Barber <wizza.burma@gmail.com>
2026-05-03 11:03:24 +0630

    Add files via upload

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
+
 // config.js
 const CONFIG = {
     USERS_SHEET_API: 'https://script.google.com/macros/s/AKfycbykNsXYHHBzHZ0-bFrFVLon1yKcMGPY2t9Z3d659BvtFlBVjzQURo8So1UKMa2wMnPO/exec',
     HISTORY_SHEET_API: 'https://script.google.com/macros/s/AKfycbxEarnFSqXxG16vLEKJ7nwbCQcGNbQTEf7a-XVzSuuEgDY5DHqcwJ4uIraqK0x-ZzYL/exec',
-    IMGBB_PROXY_URL: 'https://script.google.com/macros/s/AKfycbwVqRvK9SoRpFz3NuTM5f5pSKUxVc5dNPNGZSXvrdbmAv4H6f2kGR-0LGHbLOGX145Erw/exec',
+    IMGBB_PROXY_URL: 'https://script.google.com/macros/s/AKfycbyAZ29zcZo31i4--jTr2Hn1RB9DEJ7IiSgXjlh6jZCu8PcOJHutqGGHunWVT68YMg7e/exec',
     IMGBB_API_KEY: 'e91fc2f91dc6bb15046d0684d17f6c6b',
-    
-    // ✅ NEW - Chat API URL (အပေါ်က Deploy လုပ်ထားတဲ့ URL ကိုထည့်)
-    CHAT_API_URL: 'https://script.google.com/macros/s/AKfycbybDbfxKvPH0_t2Xm1PLaiNXlJrPQa4_nIb041waHcyjLGt4HMXHmvg2d2Itk9fnzt0pw/exec',
-    
     AUTH_EXPIRY_KEY: 'hair_auth_expiry',
     USER_DATA_KEY: 'hair_user_data',
-    LOGIN_DURATION_MS: 86400000
+    LOGIN_DURATION_MS: 86400000,
+    
+    // ✅ ဒီအပိုင်းကို ထပ်ထည့်ပါ (မူရင်းအတိုင်း)
+    CHAT_API_URL: 'https://script.google.com/macros/s/AKfycbwY-IHvkGbmKpzaZTqUv7ga8d-Hcd6jUo31wIcupIFa2Bvkvf75kEF5brmavlIqFwkc/exec'  // ဒီနေရာမှာ မင်း Deploy ရထားတဲ့ URL ထည့်
 };
